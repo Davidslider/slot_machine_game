f91afd9538d57a0f8477597e2dd14b4456872335
Davidslider <46541772+Davidslider@users.noreply.github.com>
2019-02-05 15:17:50 -0600

    made changes to cells when they match, added reset button

--- js/script.js
+++ js/script.js
@@ -13,20 +13,38 @@ let matches = 0
 
 
 /*----- cached element references -----*/
+const spinners = document.getElementsByClassName('spinner')
 const button = document.getElementById('spin-btn')
+const resetBtn = document.getElementById('reset-btn')
 const cell1 = document.getElementById('cell1')
 const cell2 = document.getElementById('cell2')
-const cell2 = document.getElementById('cell3')
+const cell3 = document.getElementById('cell3')
+
 const message = document.getElementById('message')
 const match = document.getElementById('matches') 
 
 
 /*----- event listeners -----*/
 button.addEventListener('click', slot)
-
+resetBtn.addEventListener('click', reset)
 
 /*----- functions -----*/
 
+function reset(){
+    button.disabled = false
+    cell1.innerHTML = 0
+    cell2.innerHTML = 0
+    cell3.innerHTML = 0
+    message.innerHTML = ''
+    match.innerHTML = ''
+    matches = 0
+    for(let i of spinners){
+        i.style.backgroundColor = "aqua"
+        i.style.color = "black"
+        i.style.fontWeight = "350"
+        i.style.fontSize = "330%"
+    }
+}
 
 
 function displayMessage() {
@@ -36,6 +54,11 @@ function displayMessage() {
 function slot() {
     cell1.innerHTML = randomNum()
     cell2.innerHTML = randomNum()
+    cell3.innerHTML = randomNum()
+    // cell1.innerHTML = 7
+    // cell2.innerHTML = 7
+    // cell3.innerHTML = 7
+    
 
     checkMatch()
 }
@@ -52,15 +75,29 @@ function slot() {
 function checkMatch() {
     let val1 = cell1.innerHTML
     let val2 = cell2.innerHTML
-    if (val1 === val2) {
+    let val3 = cell3.innerHTML
+    if (val1 === val2 && val1 === val3 && val2 === val3) {
         message.innerHTML = "Winner!!"
-        matches = matches + 1 
+        matches = matches + 1
         match.innerHTML = matches
-        // match.innerHTML = (matches + 1)
-        // Update element in the DOM
-        // match.innerHTML = (matches + 1)
+     
+        // button.disabled = true
+        for(let i of spinners){
+            i.style.backgroundColor = "purple"
+            i.style.color = "white"
+            i.style.fontWeight = "900"
+            i.style.fontSize = "500%"
+        }
+        
     } else {
         message.innerHTML = "Try Again!"
+        for(let i of spinners){
+            i.style.backgroundColor = "aqua"
+            i.style.color = "black"
+            i.style.fontWeight = "350"
+            i.style.fontSize = "330%"
+            
+        }
     }
 
 
